c0d83e4e652055078035080200bb3ec70776643f
ksh <kangshqwert@naver.com>
2024-03-05 13:18:48 +0900

    [기능추가]: 폴더 페이지 로딩 시 스켈레톤 추가

--- src/components/templates/FolderPostPage/FolderPostPage.tsx
+++ src/components/templates/FolderPostPage/FolderPostPage.tsx
@@ -3,7 +3,11 @@ import { usePost } from '@/src/stores/Post/usePost';
 
 import { Box } from '../../atoms/Box';
 import { Header } from '../../organisms/Header';
-import { PostCard, PostCardBack } from '../../organisms/PostCard';
+import {
+  PostCard,
+  PostCardBack,
+  PostCardSkeleton,
+} from '../../organisms/PostCard';
 import * as styles from './folderPostPage.css';
 import { useFolderPost } from './useFolderPost';
 
@@ -20,6 +24,7 @@ export const FolderPostPage = () => {
     path,
     pathArray,
     session,
+    getDirectoryLoading,
   } = useFolderPost();
 
   const { isActive, setIsActive } = usePost();
@@ -46,63 +51,79 @@ export const FolderPostPage = () => {
         justifyContent="center"
       >
         <Box className={styles.cardContainer({ direction: isActive })}>
-          {pathArray.length > 3 && (
-            <Box width={tree?.length === 0 ? '96' : 'full'}>
-              <PostCardBack
-                variant="folder"
-                path={pathBack}
-                name="../"
-                direction={isActive}
-              />
-            </Box>
-          )}
-          {tree &&
-            tree.map((item, idx) => (
-              <Box
-                key={idx}
-                position="relative"
-                onMouseEnter={() => {
-                  !isOpen && setHover(idx);
-                }}
-                onMouseLeave={() => {
-                  !isOpen && setHover(-1);
-                }}
-              >
-                {item.type === 'folder' ? (
-                  <PostCard
+          {getDirectoryLoading ? (
+            <>
+              <PostCardSkeleton />
+              <PostCardSkeleton />
+              <PostCardSkeleton />
+              <PostCardSkeleton />
+              <PostCardSkeleton />
+              <PostCardSkeleton />
+              <PostCardSkeleton />
+              <PostCardSkeleton />
+              <PostCardSkeleton />
+            </>
+          ) : (
+            <>
+              {pathArray.length > 3 && (
+                <Box width={tree.length === 0 ? '96' : 'full'}>
+                  <PostCardBack
                     variant="folder"
-                    path={item.path}
-                    name={item.name}
+                    path={pathBack}
+                    name="../"
                     direction={isActive}
-                    own={own}
-                    hover={hover}
-                    idx={idx}
-                    isOpen={isOpen}
-                    setIsOpen={setIsOpen}
                   />
-                ) : (
-                  <PostCard
-                    variant={
-                      session?.user.dirName === item.path.split('/', 1)[0]
-                        ? 'cardInFolder'
-                        : 'card'
-                    }
-                    path={item.path}
-                    name={item.name.replaceAll('.md', '')}
-                    userName={item.userName}
-                    thumbnail={item.thumbnail ?? ''}
-                    subTitle={item.subTitle}
-                    date={item.date}
-                    direction={isActive}
-                    own={own}
-                    hover={hover}
-                    idx={idx}
-                    isOpen={isOpen}
-                    setIsOpen={setIsOpen}
-                  />
-                )}
-              </Box>
-            ))}
+                </Box>
+              )}
+              {tree &&
+                tree.map((item, idx) => (
+                  <Box
+                    key={idx}
+                    position="relative"
+                    onMouseEnter={() => {
+                      !isOpen && setHover(idx);
+                    }}
+                    onMouseLeave={() => {
+                      !isOpen && setHover(-1);
+                    }}
+                  >
+                    {item.type === 'folder' ? (
+                      <PostCard
+                        variant="folder"
+                        path={item.path}
+                        name={item.name}
+                        direction={isActive}
+                        own={own}
+                        hover={hover}
+                        idx={idx}
+                        isOpen={isOpen}
+                        setIsOpen={setIsOpen}
+                      />
+                    ) : (
+                      <PostCard
+                        variant={
+                          session?.user.dirName === item.path.split('/', 1)[0]
+                            ? 'cardInFolder'
+                            : 'card'
+                        }
+                        path={item.path}
+                        name={item.name.replaceAll('.md', '')}
+                        userName={item.userName}
+                        thumbnail={item.thumbnail ?? ''}
+                        subTitle={item.subTitle}
+                        date={item.date}
+                        direction={isActive}
+                        own={own}
+                        hover={hover}
+                        idx={idx}
+                        isOpen={isOpen}
+                        setIsOpen={setIsOpen}
+                      />
+                    )}
+                  </Box>
+                ))}
+            </>
+          )}
           {/* 추후 로딩시 스켈레톤 추가 */}
           {/* TODO: 추후 패치 loading을 이용해서 skeleton 삽입 */}
           {/* <PostCardSkeleton />
