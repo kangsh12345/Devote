01e7323c460ddc4174370960579b7e96104450e4
ksh <kangshqwert@naver.com>
2024-03-12 11:09:14 +0900

    [기능추가]: 이름 수정 mutate 프리패치 기능 추가

--- src/hooks/api/post/useRenameMutation.ts
+++ src/hooks/api/post/useRenameMutation.ts
@@ -1,5 +1,11 @@
 import { httpPostClient } from '@/src/utils/client';
-import { useMutation } from '@tanstack/react-query';
+import { TreeProps } from '@/src/utils/fs';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import {
+  GetAllDirectoryQueryKey,
+  GetAllDirectoryQueryResponse,
+} from './useGetAllDirectoryQuery';
 
 export const RenameMutationKey = 'api/post/rename';
 
@@ -13,11 +19,79 @@ export interface RenameMutationResponse {
 }
 
 export function useRenameMutation() {
+  const queryClient = useQueryClient();
+
   return useMutation(
     [RenameMutationKey],
     async (path: RenameMutationRequest) =>
       await httpPostClient<RenameMutationResponse>(RenameMutationKey, path),
+
+    {
+      onMutate: async ({ path, newPath }) => {
+        await queryClient.cancelQueries([GetAllDirectoryQueryKey]);
+
+        const previous = queryClient.getQueryData([GetAllDirectoryQueryKey]);
+
+        queryClient.setQueryData<GetAllDirectoryQueryResponse>(
+          [GetAllDirectoryQueryKey],
+          old => {
+            path = path.replace('.md', '');
+
+            const newTree: TreeProps | undefined = old
+              ? old.tree
+              : { path: '', name: '', type: 'folder', children: [] };
+
+            const renameItem = (
+              t: TreeProps,
+              targetPath: string,
+              newNamePath: string,
+            ) => {
+              if (t.path === targetPath) {
+                return {
+                  ...t,
+                  path: newNamePath,
+                  name: newNamePath.split('/').pop()?.replace('.md', '') || '',
+                };
+              } else {
+                const updatedChildren: TreeProps[] = t.children.map(child =>
+                  renameItem(child, targetPath, newNamePath),
+                );
+                return {
+                  ...t,
+                  children: updatedChildren,
+                };
+              }
+            };
+
+            const change = renameItem(
+              newTree ?? { path: '', name: '', type: 'folder', children: [] },
+              path,
+              newPath,
+            );
+
+            const newResult: GetAllDirectoryQueryResponse = {
+              success: old?.success ?? true,
+              tree: {
+                path: old?.tree?.path ?? '',
+                name: old?.tree?.name ?? '',
+                type: old?.tree?.type ?? 'folder',
+                children: change.children,
+              },
+              message: old?.message ?? 'success',
+            };
+
+            return newResult;
+          },
+        );
+
+        return { previous };
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries([GetAllDirectoryQueryKey]);
+      },
+      onError: (__, _, context) => {
+        queryClient.setQueryData([GetAllDirectoryQueryKey], context?.previous);
+      },
+    },
   );
 }
-
-// TODO: Rename시 데이터 프리패칭할 수 있도록 변경
