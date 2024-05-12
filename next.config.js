af7f27d54dbaa18ea41adebb0c0e22f385a573cb
ksh <kangshqwert@naver.com>
2024-05-12 21:20:41 +0900

    [기타]: CORS 에러 테스트

--- next.config.js
+++ next.config.js
@@ -29,9 +29,33 @@ const nextConfig = {
         source: '/v0/:path*',
         destination: 'https://firebasestorage.googleapis.com/v0/:path*',
       },
+    ];
+  },
+  async headers() {
+    return [
       {
-        source: '/api/:path*',
-        destination: 'https://devote-blog.vercel.app/api/:path*',
+        // Routes this applies to
+        source: '/api/(.*)',
+        // Headers
+        headers: [
+          // Allow for specific domains to have access or * for all
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+          // Allows for specific methods accepted
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
       },
     ];
   },
