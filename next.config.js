d747a2b6d637341dea0e5ce8d22df2ed7e41b850
ksh <kangshqwert@naver.com>
2024-05-12 21:34:44 +0900

    [기타]: CORS 에러 수정전 복귀

--- next.config.js
+++ next.config.js
@@ -29,38 +29,6 @@ const nextConfig = {
         source: '/v0/:path*',
         destination: 'https://firebasestorage.googleapis.com/v0/:path*',
       },
-      {
-        source: '/api/:path*',
-        destination: 'https://your-api-server.com/:path*',
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        // Routes this applies to
-        source: '/api/(.*)',
-        // Headers
-        headers: [
-          // Allow for specific domains to have access or * for all
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-            // DOES NOT WORK
-            // value: process.env.ALLOWED_ORIGIN,
-          },
-          // Allows for specific methods accepted
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          // Allows for specific headers accepted (These are a few standard ones)
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-        ],
-      },
     ];
   },
 };
